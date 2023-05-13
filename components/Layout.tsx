276bc952af763c5e3e99afc6016c0ad21114a956
Andronzi <andrey.radioniv.2003@gmail.com>
2023-05-13 11:43:59 +0700

    some minor changes in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,54 +4,56 @@ import { FC, PropsWithChildren } from "react";
 
 const Layout: FC<PropsWithChildren<unknown>> = ({ children }) => {
   return (
-    <div>
-      <nav className="max-w-7xl mx-auto flex justify-between items-center">
-        <ul className="flex items-center pl-0 py-3">
-          <Link className="nav__link text-sm text-black" href="/">
-            <Image src="/logo.svg" alt="Логотип" width={180} height={60} />
-          </Link>
-          <Link className="nav__link text-sm text-dark ml-8" href="/events">
-            Мероприятия
-          </Link>
-          <Link className="nav__link text-sm text-dark ml-4" href="/events">
-            Бронь
-          </Link>
-          <Link className="nav__link text-sm text-dark ml-4" href="/events">
-            Сотрудники
-          </Link>
-          <Link className="nav__link text-sm text-dark ml-4" href="/events">
-            Магазин
-          </Link>
-        </ul>
-        <div className="flex">
-          <div className="search relative">
+    <div className="bg-dark">
+      <div className="border-b-2 border-gray-100/10">
+        <nav className="max-w-7xl mx-auto flex justify-between items-center">
+          <ul className="flex items-center pl-0 py-3">
+            <Link className="nav__link text-black text-lg" href="/">
+              <Image src="/logo.svg" alt="Логотип" width={180} height={60} />
+            </Link>
+            <Link className="nav__link text-md text-white ml-12" href="/events">
+              Мероприятия
+            </Link>
+            <Link className="nav__link text-md text-white ml-6" href="/events">
+              Сотрудники
+            </Link>
+            <Link className="nav__link text-md text-white ml-6" href="/booking">
+              Бронь
+            </Link>
+            <Link className="nav__link text-md text-white ml-6" href="/market">
+              Магазин
+            </Link>
+          </ul>
+          <div className="flex">
+            <div className="search relative">
+              <Image
+                className="absolute top-2.5 left-3"
+                src="/search.svg"
+                alt="Аватар"
+                width={24}
+                height={24}
+              />
+              <input
+                className="border-2 border-grey rounded-lg p-2 pl-10 focus:outline-none focus:ring-2 ring-white"
+                type="search"
+                placeholder="Search..."
+              />
+            </div>
+            <div className="flex items-center ml-6">
+              <Image src="/coin.svg" alt="Монетка" width={28} height={28} />
+              <p className="text-white text-lg">50</p>
+            </div>
             <Image
-              className="absolute top-2.5 left-3"
-              src="/search.svg"
+              className="profile ml-6"
+              src="/avatar.svg"
               alt="Аватар"
-              width={24}
-              height={24}
-            />
-            <input
-              className="border-2 border-grey rounded-lg p-2 pl-10 focus:outline-none focus:ring-1"
-              type="search"
-              placeholder="Search..."
+              width={36}
+              height={36}
             />
           </div>
-          <div className="flex items-center ml-6">
-            <Image src="/coin.svg" alt="Монетка" width={28} height={28} />
-            <p className="text-dark text-lg">50</p>
-          </div>
-          <Image
-            className="profile ml-6"
-            src="/avatar.svg"
-            alt="Аватар"
-            width={36}
-            height={36}
-          />
-        </div>
-      </nav>
-      {children}
+        </nav>
+      </div>
+      <div className="max-w-7xl mx-auto">{children}</div>
     </div>
   );
 };
