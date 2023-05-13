94e1692b24ae06ba0ee03670f5cccaa41fb3ca90
Alexander Zorkin <somnoynadno@yandex.ru>
2023-05-13 16:35:26 +0300

    Fix m2m

--- services/employee/types.ts
+++ services/employee/types.ts
@@ -1,7 +1,11 @@
+import {IUnit} from "../unit/types";
+
 export interface IEmployee {
     ID?: string;
     Name: string;
     Balance: number;
+
+    EmployeeUnits: {Unit: IUnit}[];
 }
 
 export type EmployeeRequestBody = Omit<IEmployee, "id">;
