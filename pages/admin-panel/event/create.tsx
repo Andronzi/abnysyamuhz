3f64120e22bb7947be4adce5f36e5926d1f164a6
Andronzi <andrey.radioniv.2003@gmail.com>
2023-05-13 23:27:29 +0700

    add margin for create form

--- pages/admin-panel/event/create.tsx
+++ pages/admin-panel/event/create.tsx
@@ -1,32 +1,30 @@
-import {
-	Card,
-	Input,
-	Checkbox,
-	Button,
-	Typography,
-} from "@material-tailwind/react";
+import { Button, Card, Input, Typography } from "@material-tailwind/react";
 
 const EventForm = () => {
-	return (
-		<Card color="white" shadow={true} className="p-8 max-w-md mx-auto rounded-lg">
-			<Typography variant="h4" className="text-center mb-6">
-				Add News
-			</Typography>
-			<Typography className="text-center mb-8">
-				Enter your details to send news.
-			</Typography>
-			<form className="flex flex-col gap-6">
-				<Input size="lg" color="blue" placeholder="Enter Name" />
-				<Input size="lg" color="blue" placeholder="Enter Description" />
-				<Input size="lg" color="blue" placeholder="Enter Place" />
-				<Input size="lg" type="date" color="blue" placeholder="Enter Data" />
-                <Input size="lg" type="time" color="blue" placeholder="Enter Time" />
-				<Button size="lg" className="mt-6 bg-blue-400">
-					Send News
-				</Button>
-			</form>
-		</Card>
-	);
+  return (
+    <Card
+      color="white"
+      shadow={true}
+      className="mt-8 p-8 max-w-md mx-auto rounded-lg"
+    >
+      <Typography variant="h4" className="text-center mb-6">
+        Add News
+      </Typography>
+      <Typography className="text-center mb-8">
+        Enter your details to send news.
+      </Typography>
+      <form className="flex flex-col gap-6">
+        <Input size="lg" color="blue" placeholder="Enter Name" />
+        <Input size="lg" color="blue" placeholder="Enter Description" />
+        <Input size="lg" color="blue" placeholder="Enter Place" />
+        <Input size="lg" type="date" color="blue" placeholder="Enter Data" />
+        <Input size="lg" type="time" color="blue" placeholder="Enter Time" />
+        <Button size="lg" className="mt-6 bg-blue-400">
+          Send News
+        </Button>
+      </form>
+    </Card>
+  );
 };
 
 export default EventForm;
