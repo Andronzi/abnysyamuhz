e774f12706980cf7a7b86ebee8e69e2e2958e34c
Andronzi <andrey.radioniv.2003@gmail.com>
2023-05-13 23:25:43 +0700

    add tag types to main api

--- services/realSplitApi.ts
+++ services/realSplitApi.ts
@@ -1,7 +1,7 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
 export const realSplitApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'http://somnoynadno.ru:8080/api/v1/' }),
-    tagTypes: ["News", "Employee", "Units"],
+    tagTypes: ["News", "Products", "Achievements", "Employee", "Events", "Units", "Tasks"],
     endpoints: () => ({}),
 })
