f87eb21d3bd4382c07f83812960c5044fd616663
Andronzi <andrey.radioniv.2003@gmail.com>
2023-05-14 06:05:29 +0700

    add news, event deletion

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,50 +1,70 @@
 import Image from "next/image";
 import { FC } from "react";
-import { convertDate } from "../helpers/getDate";
+import toast from "react-hot-toast";
 import { IEvent } from "../pages/api/models/event";
-
+import { useDeleteEventMutation } from "../services/event/eventRealApi";
 const EventCard: FC<IEvent & { isAdmin: boolean }> = ({
+  ID,
   Name,
   ImageUrl,
   Place,
   Date,
   isAdmin,
-}) => (
-  <>
-    <p className="text-white text-2xl font-bold">{Name}</p>
-    <img
-      className="mt-2 object-cover h-44 w-full rounded-md"
-      src={ImageUrl}
-      alt="Картинка мероприятия"
-    />
-    <p className="text-white text-md mt-2 flex">
-      <Image src="/place.svg" alt="Место проведения" width={20} height={20} />
-      <span className="ml-2">{Place}</span>
-    </p>
-    <p className="text-white text-md flex">
-      <Image src="/calendar.svg" alt="Дата проведения" width={20} height={20} />
-      <span className="ml-2">{Date}</span>
-    </p>
-    <p className="text-white text-md flex">
-      <Image src="/price.svg" alt="Приз" width={20} height={20} />
-      <span className="ml-2">1000</span>
-    </p>
-    {!isAdmin && (
-      <button className="w-full py-1.5 mt-2 bg-coral text-white rounded-md font-medium">
-        Принять участие
-      </button>
-    )}
-    {isAdmin && (
-      <div className="flex gap-4">
-        <button className="w-full py-1.5 mt-2 bg-amber-500	 text-white rounded-md font-medium">
-          Изменить
+}) => {
+  const [deleteEvent] = useDeleteEventMutation();
+  return (
+    <>
+      <p className="text-white text-2xl font-bold">{Name}</p>
+      <img
+        className="mt-2 object-cover h-44 w-full rounded-md"
+        src={ImageUrl}
+        alt="Картинка мероприятия"
+      />
+      <p className="text-white text-md mt-2 flex">
+        <Image src="/place.svg" alt="Место проведения" width={20} height={20} />
+        <span className="ml-2">{Place}</span>
+      </p>
+      <p className="text-white text-md flex">
+        <Image
+          src="/calendar.svg"
+          alt="Дата проведения"
+          width={20}
+          height={20}
+        />
+        <span className="ml-2">{Date}</span>
+      </p>
+      <p className="text-white text-md flex">
+        <Image src="/price.svg" alt="Приз" width={20} height={20} />
+        <span className="ml-2">1000</span>
+      </p>
+      {!isAdmin && (
+        <button className="w-full py-1.5 mt-2 bg-coral text-white rounded-md font-medium">
+          Принять участие
         </button>
-        <button className="w-full py-1.5 mt-2 bg-red-500 text-white rounded-md font-medium">
-          Удалить
-        </button>
-      </div>
-    )}
-  </>
-);
+      )}
+      {isAdmin && (
+        <div className="flex gap-4">
+          <button className="w-full py-1.5 mt-2 bg-amber-500	 text-white rounded-md font-medium">
+            Изменить
+          </button>
+          <button
+            className="w-full py-1.5 mt-2 bg-red-500 text-white rounded-md font-medium"
+            onClick={() => {
+              try {
+                deleteEvent(ID);
+                toast.success("Мероприятие успешно удалено");
+              } catch (err) {
+                console.log(err);
+                toast.error("Упс... Что-то пошло не так :(");
+              }
+            }}
+          >
+            Удалить
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default EventCard;
--- components/News.tsx
+++ components/News.tsx
@@ -1,32 +1,49 @@
 import { FC } from "react";
+import toast from "react-hot-toast";
 import { News } from "../pages/api/models/news";
+import { useDeleteNewsMutation } from "../services/news/newsRealApi";
 
 const NewsCard: FC<News & { isAdmin: boolean }> = ({
+  ID,
   ImageUrl,
   Title,
   Body,
   isAdmin,
-}) => (
-  <div className="cursor-pointer">
-    <img
-      className="object-cover h-44 w-full rounded-md"
-      src={ImageUrl}
-      alt="Картинка новости"
-    />
-    <div className="info py-2">
-      <p className="text-xl text-white font-bold">{Title}</p>
-      <p className="mt-3 text-white text-base font-regular h-20">{Body}</p>
-      {!!isAdmin && (
-        <div className="flex gap-4 items-center mt-2">
-          <button className="w-full py-1.5 bg-amber-500 text-white rounded-md font-medium p-3">
-            Изменить
-          </button>
-          <button className="w-full py-1.5 bg-red-500 text-white rounded-md font-medium p-3">
-            Удалить
-          </button>
-        </div>
-      )}
+}) => {
+  const [deleteNews] = useDeleteNewsMutation();
+  return (
+    <div className="cursor-pointer">
+      <img
+        className="object-cover h-44 w-full rounded-md"
+        src={ImageUrl}
+        alt="Картинка новости"
+      />
+      <div className="info py-2">
+        <p className="text-xl text-white font-bold">{Title}</p>
+        <p className="mt-3 text-white text-base font-regular h-20">{Body}</p>
+        {!!isAdmin && (
+          <div className="flex gap-4 items-center mt-2">
+            <button className="w-full py-1.5 bg-amber-500 text-white rounded-md font-medium p-3">
+              Изменить
+            </button>
+            <button
+              className="w-full py-1.5 bg-red-500 text-white rounded-md font-medium p-3"
+              onClick={() => {
+                try {
+                  deleteNews(ID).unwrap();
+                  toast.success("Новость успешно удалена");
+                } catch (err) {
+                  console.log(err);
+                  toast.error("Упс... Что-то пошло не так :(");
+                }
+              }}
+            >
+              Удалить
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default NewsCard;
