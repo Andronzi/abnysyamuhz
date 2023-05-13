ff020d3e210b607345163561c110c86308ebf638
Ivan Arsenev <76220709+IvanArsenev@users.noreply.github.com>
2023-05-13 23:52:27 +0700

    Add files via upload

--- pages/admin-panel/event/edit.tsx
+++ pages/admin-panel/event/edit.tsx
@@ -7,7 +7,11 @@ import {
 } from "@material-tailwind/react";
 
 const EventForm = () => {
-	
+	var oldName = 'namename';
+	var oldDescription = 'descdesc';
+	var oldPrice = '10.99';
+
+
 	return (
 		<Card color="white" shadow={true} className="p-8 max-w-md mx-auto rounded-lg">
 			<Typography variant="h4" className="text-center mb-6">
@@ -17,11 +21,11 @@ const EventForm = () => {
 				Enter your details to send news.
 			</Typography>
 			<form className="flex flex-col gap-6">
-				<Input size="lg" color="blue" placeholder="Enter Name" />
-				<Input size="lg" color="blue" placeholder="Enter Description" />
-				<Input size="lg" color="blue" placeholder="Enter Place" />
-				<Input size="lg" type="date" color="blue" placeholder="Enter Data" />
-                <Input size="lg" type="time" color="blue" placeholder="Enter Time" />
+				<Input size="lg" color="blue" placeholder="Enter Name" value={oldName}/>
+				<Input size="lg" color="blue" placeholder="Enter Description" value={oldDescription}/>
+				<Input size="lg" color="blue" placeholder="Enter Place" value={oldPrice}/>
+				<Input size="lg" type="date" color="blue" placeholder="Enter Data"/>
+				<Input size="lg" type="time" color="blue" placeholder="Enter Time"/>
 				<Button size="lg" className="mt-6 bg-blue-400">
 					Send News
 				</Button>
