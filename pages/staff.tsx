967e97a047665035539114f30f026b97fb97107c
IvanArsenev <arsenev.ivan00@bk.ru>
2023-05-14 15:09:31 +0700

    accordioncoins

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -31,205 +31,205 @@ const Staff = () => {
 			</div>
 
 			<div className="p-4 rounded-lg" style={{ backgroundColor: "#F2F2F7" }}>
-				<Accordion open={isOpen(2)}>
-					<AccordionHeader onClick={() => handleOpen(2)}>
-						Офис г. Санкт-Петербург
-					</AccordionHeader>
-					<AccordionBody className={"p-2"}>
-						<Accordion open={isOpen(22)}>
-							<AccordionHeader onClick={() => handleOpen(22)}>
-								Отдел разработки
-							</AccordionHeader>
+					<Accordion open={isOpen(2)}>
+						<AccordionHeader onClick={() => handleOpen(2)}>
+							Офис г. Санкт-Петербург
+						</AccordionHeader>
+						<AccordionBody className={"p-2"}>
+							<Accordion open={isOpen(22)}>
+								<AccordionHeader onClick={() => handleOpen(22)}>
+									Отдел разработки
+								</AccordionHeader>
 
-							<AccordionBody className={"p-2"}>
+								<AccordionBody className={"p-2"}>
 
-								<Accordion open={isOpen(221)}>
-									<AccordionHeader onClick={() => handleOpen(221)}>
-										Команда разработки фронтенда
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Иванов Иван Иванович
-										<br/>
-										Смирнова Анастасия Петровна
-										<br/>
-										Королева Екатерина Дмитриевна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(221)}>
+										<AccordionHeader onClick={() => handleOpen(221)}>
+											Команда разработки фронтенда
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Иванов Иван Иванович</span><span className="balance">1235 coins</span>
+											<br/>
+											<span className="link">Смирнова Анастасия Петровна</span><span className="balance">5320 coins</span>
+											<br/>
+											<span className="link">Королева Екатерина Дмитриевна</span><span className="balance">650 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(222)}>
-									<AccordionHeader onClick={() => handleOpen(222)}>
-										Команда разработки бэкенда
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Петров Петр Петрович
-										<br/>
-										Сидоров Сидор Сидорович
-										<br/>
-										Федоров Дмитрий Иванович
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(222)}>
+										<AccordionHeader onClick={() => handleOpen(222)}>
+											Команда разработки бэкенда
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Петров Петр Петрович</span><span className="balance">340 coins</span>
+											<br/>
+											<span className="link">Сидоров Сидор Сидорович</span><span className="balance">720 coins</span>
+											<br/>
+											<span className="link">Федоров Дмитрий Иванович</span><span className="balance">830 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(223)}>
-									<AccordionHeader onClick={() => handleOpen(223)}>
-										Команда тестирования
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Новикова Анастасия Александровна
-										<br/>
-										Кузнецов Алексей Владимирович
-										<br/>
-										Соколова Ольга Игоревна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(223)}>
+										<AccordionHeader onClick={() => handleOpen(223)}>
+											Команда тестирования
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Новикова Анастасия Александровна</span><span className="balance">1360 coins</span>
+											<br/>
+											<span className="link">Кузнецов Алексей Владимирович</span><span className="balance">120 coins</span>
+											<br/>
+											<span className="link">Соколова Ольга Игоревна</span><span className="balance">540 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-							</AccordionBody>
-						</Accordion>
+								</AccordionBody>
+							</Accordion>
 
-						<Accordion open={isOpen(4)}>
-							<AccordionHeader onClick={() => handleOpen(4)}>
-								Отдел дизайна
-							</AccordionHeader>
+							<Accordion open={isOpen(4)}>
+								<AccordionHeader onClick={() => handleOpen(4)}>
+									Отдел дизайна
+								</AccordionHeader>
 
-							<AccordionBody className={"p-2"}>
+								<AccordionBody className={"p-2"}>
 
-								<Accordion open={isOpen(41)}>
-									<AccordionHeader onClick={() => handleOpen(41)}>
-										Команда дизайнеров интерфейсов
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Смирнова Елена Владимировна
-										<br/>
-										Попов Андрей Сергеевич
-										<br/>
-										Козлова Анна Максимовна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(41)}>
+										<AccordionHeader onClick={() => handleOpen(41)}>
+											Команда дизайнеров интерфейсов
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Смирнова Елена Владимировна</span><span className="balance">450 coins</span>
+											<br/>
+											<span className="link">Попов Андрей Сергеевич</span><span className="balance">730 coins</span>
+											<br/>
+											<span className="link">Козлова Анна Максимовна</span><span className="balance">630 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(42)}>
-									<AccordionHeader onClick={() => handleOpen(42)}>
-										Команда графических дизайнеров
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Ковалев Иван Сергеевич
-										<br/>
-										Михайлова Оксана Александровна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(42)}>
+										<AccordionHeader onClick={() => handleOpen(42)}>
+											Команда графических дизайнеров
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Ковалев Иван Сергеевич</span><span className="balance">420 coins</span>
+											<br/>
+											<span className="link">Михайлова Оксана Александровна</span><span className="balance">580 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-							</AccordionBody>
-						</Accordion>
+								</AccordionBody>
+							</Accordion>
 
-						<Accordion open={isOpen(5)}>
-							<AccordionHeader onClick={() => handleOpen(5)}>
-								Отдел продукта
-							</AccordionHeader>
+							<Accordion open={isOpen(5)}>
+								<AccordionHeader onClick={() => handleOpen(5)}>
+									Отдел продукта
+								</AccordionHeader>
 
-							<AccordionBody className={"p-2"}>
+								<AccordionBody className={"p-2"}>
 
-								<Accordion open={isOpen(51)}>
-									<AccordionHeader onClick={() => handleOpen(51)}>
-										Продуктовый менеджер
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Козлов Максим Сергеевич
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(51)}>
+										<AccordionHeader onClick={() => handleOpen(51)}>
+											Продуктовый менеджер
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Козлов Максим Сергеевич</span><span className="balance">750 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(52)}>
-									<AccordionHeader onClick={() => handleOpen(52)}>
-										Команда аналитики
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Иванова Ольга Викторовна
-										<br/>
-										Соколов Сергей Андреевич
-										<br/>
-										Петрова Екатерина Дмитриевна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(52)}>
+										<AccordionHeader onClick={() => handleOpen(52)}>
+											Команда аналитики
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Иванова Ольга Викторовна</span><span className="balance">750 coins</span>
+											<br/>
+											<span className="link">Соколов Сергей Андреевич</span><span className="balance">950 coins</span>
+											<br/>
+											<span className="link">Петрова Екатерина Дмитриевна</span><span className="balance">1740 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(53)}>
-									<AccordionHeader onClick={() => handleOpen(53)}>
-										Команда менеджмента продукта
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Сидорова Анастасия Ивановна
-										<br/>
-										Федорова Оксана Владимировна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(53)}>
+										<AccordionHeader onClick={() => handleOpen(53)}>
+											Команда менеджмента продукта
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Сидорова Анастасия Ивановна</span><span className="balance">420 coins</span>
+											<br/>
+											<span className="link">Федорова Оксана Владимировна</span><span className="balance">750 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-							</AccordionBody>
-						</Accordion>
+								</AccordionBody>
+							</Accordion>
 
-						<Accordion open={isOpen(6)}>
-							<AccordionHeader onClick={() => handleOpen(6)}>
-								Отдел маркетинга
-							</AccordionHeader>
+							<Accordion open={isOpen(6)}>
+								<AccordionHeader onClick={() => handleOpen(6)}>
+									Отдел маркетинга
+								</AccordionHeader>
 
-							<AccordionBody className={"p-2"}>
+								<AccordionBody className={"p-2"}>
 
-								<Accordion open={isOpen(61)}>
-									<AccordionHeader onClick={() => handleOpen(61)}>
-										Команда маркетологов
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Никитина Анна Александровна
-										<br/>
-										Михайлов Игорь Васильевич
-										<br/>
-										Смирнов Денис Сергеевич
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(61)}>
+										<AccordionHeader onClick={() => handleOpen(61)}>
+											Команда маркетологов
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Никитина Анна Александровна</span><span className="balance">730 coins</span>
+											<br/>
+											<span className="link">Михайлов Игорь Васильевич</span><span className="balance">340 coins</span>
+											<br/>
+											<span className="link">Смирнов Денис Сергеевич</span><span className="balance">620 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(62)}>
-									<AccordionHeader onClick={() => handleOpen(62)}>
-										Команда PR
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Кузнецова Елена Андреевна
-										<br/>
-										Попова Анна Сергеевна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(62)}>
+										<AccordionHeader onClick={() => handleOpen(62)}>
+											Команда PR
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Кузнецова Елена Андреевна</span><span className="balance">630 coins</span>
+											<br/>
+											<span className="link">Попова Анна Сергеевна</span><span className="balance">850 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-							</AccordionBody>
-						</Accordion>
+								</AccordionBody>
+							</Accordion>
 
-						<Accordion open={isOpen(7)}>
-							<AccordionHeader onClick={() => handleOpen(7)}>
-								Отдел кадров
-							</AccordionHeader>
+							<Accordion open={isOpen(7)}>
+								<AccordionHeader onClick={() => handleOpen(7)}>
+									Отдел кадров
+								</AccordionHeader>
 
-							<AccordionBody className={"p-2"}>
+								<AccordionBody className={"p-2"}>
 
-								<Accordion open={isOpen(71)}>
-									<AccordionHeader onClick={() => handleOpen(71)}>
-										Специалисты по подбору персонала
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Иванова Екатерина Сергеевна
-										<br/>
-										Петрова Наталья Андреевна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(71)}>
+										<AccordionHeader onClick={() => handleOpen(71)}>
+											Специалисты по подбору персонала
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Иванова Екатерина Сергеевна</span><span className="balance">70 coins</span>
+											<br/>
+											<span className="link">Петрова Наталья Андреевна</span><span className="balance">180 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-								<Accordion open={isOpen(72)}>
-									<AccordionHeader onClick={() => handleOpen(72)}>
-										HR-менеджеры
-									</AccordionHeader>
-									<AccordionBody className={"p-2"}>
-										Соколова Анна Петровна
-										<br/>
-										Федорова Татьяна Ивановна
-									</AccordionBody>
-								</Accordion>
+									<Accordion open={isOpen(72)}>
+										<AccordionHeader onClick={() => handleOpen(72)}>
+											HR-менеджеры
+										</AccordionHeader>
+										<AccordionBody className={"p-2"}>
+											<span className="link">Соколова Анна Петровна</span><span className="balance">740 coins</span>
+											<br/>
+											<span className="link">Федорова Татьяна Ивановна</span><span className="balance">120 coins</span>
+										</AccordionBody>
+									</Accordion>
 
-							</AccordionBody>
-						</Accordion>
+								</AccordionBody>
+							</Accordion>
 
-					</AccordionBody>
-				</Accordion>
+						</AccordionBody>
+					</Accordion>
 
 				<Accordion open={isOpen(5)}>
 					<AccordionHeader onClick={() => handleOpen(5)}>
@@ -247,9 +247,9 @@ const Staff = () => {
 										Команда разработки фронтенда
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Иван Арсеньев
+										<span className="link">Иван Арсеньев</span><span className="balance">120 coins</span>
 										<br/>
-										Андрей Радионов
+										<span className="link">Андрей Радионов</span><span className="balance">240 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -258,9 +258,9 @@ const Staff = () => {
 										Команда разработки бэкенда
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Егор Шамов
+										<span className="link">Егор Шамов</span><span className="balance">5420 coins</span>
 										<br/>
-										Александр Зоркин
+										<span className="link">Александр Зоркин</span><span className="balance">5230 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -277,7 +277,7 @@ const Staff = () => {
 										Главный дизайнер
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Карина Шуман
+										<span className="link">Карина Шуман</span><span className="balance">1230 coins</span>
 									</AccordionBody>
 								</Accordion>
 							</AccordionBody>
@@ -303,7 +303,7 @@ const Staff = () => {
 										Команда разработки фронтенда
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Иванов Иван Иванович
+										<span className="link">Иванов Иван Иванович</span><span className="balance">120 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -312,9 +312,9 @@ const Staff = () => {
 										Команда разработки бэкенда
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Петров Петр Петрович
+										<span className="link">Петров Петр Петрович</span><span className="balance">430 coins</span>
 										<br/>
-										Сидоров Сидор Сидорович
+										<span className="link">Сидоров Сидор Сидорович</span><span className="balance">40 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -323,9 +323,9 @@ const Staff = () => {
 										Команда тестирования
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Новикова Анастасия Александровна
+										<span className="link">Новикова Анастасия Александровна</span><span className="balance">640 coins</span>
 										<br/>
-										Кузнецов Алексей Владимирович
+										<span className="link">Кузнецов Алексей Владимирович</span><span className="balance">2630 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -342,9 +342,9 @@ const Staff = () => {
 									Команда дизайнеров интерфейсов
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Смирнова Елена Владимировна
+										<span className="link">Смирнова Елена Владимировна</span><span className="balance">10 coins</span>
 										<br/>
-										Попов Андрей Сергеевич
+										<span className="link">Попов Андрей Сергеевич</span><span className="balance">750 coins</span>
 									</AccordionBody>
 								</Accordion>
 							</AccordionBody>
@@ -361,7 +361,7 @@ const Staff = () => {
 										Продуктовый менеджер
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Козлов Максим Сергеевич
+										<span className="link">Козлов Максим Сергеевич</span><span className="balance">5730 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -370,9 +370,9 @@ const Staff = () => {
 										Команда аналитики
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Иванова Ольга Викторовна
+										<span className="link">Иванова Ольга Викторовна</span><span className="balance">120 coins</span>
 										<br/>
-										Соколов Сергей Андреевич
+										<span className="link">Соколов Сергей Андреевич</span><span className="balance">450 coins</span>
 									</AccordionBody>
 								</Accordion>
 
@@ -390,9 +390,9 @@ const Staff = () => {
 										Команда маркетологов
 									</AccordionHeader>
 									<AccordionBody className={"p-2"}>
-										Никитина Анна Александровна
+										<span className="link">Никитина Анна Александровна</span><span className="balance">640 coins</span>
 										<br/>
-										Михайлов Игорь Васильевич
+										<span className="link">Михайлов Игорь Васильевич</span><span className="balance">420 coins</span>
 									</AccordionBody>
 								</Accordion>
 
