2387378ea1d533f3600e942e3be7c9ef4bd6256b
Alexander Zorkin <somnoynadno@yandex.ru>
2023-05-14 12:16:19 +0300

    Fix news

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -4,17 +4,20 @@ import { useGetNewsSingleQuery } from "../../services/news/newsRealApi";
 
 const EventPage = () => {
   const { query } = useRouter();
-  const news = useGetNewsSingleQuery(query.id?.toString() || "1").data;
+  const news = useGetNewsSingleQuery(query.id?.toString() || "1")?.data;
   return (
     <>
       <div className="relative">
         <img
           className="object-cover w-full h-[34rem] z-10"
-          src="https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          src={news?.ImageUrl}
         />
-        <h1 className="absolute text-coral text-6xl font-bold top-4 left-4">
-          IT-REVOLUTION
+        <h1 className="absolute text-coral text-6xl font-bold top-4 left-4 drop-shadow-lg">
+          {news?.Title}
         </h1>
+        <p className="text-white mt-6">
+          {news?.Body}{" "}
+        </p>
       </div>
       <div className="flex flex-row justify-between">
         <div className="basis-2/5 z-20 ml-1">
@@ -31,16 +34,12 @@ const EventPage = () => {
             </button>
           )}
           <p className="text-white -mt-12">
-            Участники получат возможность узнать : о последних тенденциях в
-            IT-индустрии о новейших технологиях и инструментах для работы. Кроме
-            того, они смогут провести время с коллегами из других отделов
-            компании, обсудить свои проекты и получить ценные советы и
-            рекомендации от опытных профессионалов.{" "}
+            {news?.Event?.Description}
           </p>
         </div>
         <div className="basis-3/5 z-20 -mt-12 ml-1">
           <p className="cringe-text text-transparent text-5xl font-bold text-right tracking-wider">
-            Открытие новых горизонтов в мире технологий
+            {news?.Event?.Name}
           </p>
         </div>
       </div>
