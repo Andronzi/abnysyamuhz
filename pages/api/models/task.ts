ed9298a4d1440f2f7d087218b5337e45bac6f055
Alexander Zorkin <somnoynadno@yandex.ru>
2023-05-14 09:14:00 +0300

    Fix

--- pages/api/models/task.ts
+++ pages/api/models/task.ts
@@ -1,10 +1,9 @@
-import { IEmployee } from "../../../services/employee/types";
+import {Employee} from "./employee";
 
 export interface Task {
     Name: string;
     Description: string;
     Reward: number;
-    ratingReward: number;
     EventID: number;
-    Employees: IEmployee[];
+    Employees: Employee[];
 }
