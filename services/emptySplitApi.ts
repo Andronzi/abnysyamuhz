79aafedb12938fbbc1a860ac2ca63dd457df9629
Alexander Zorkin <somnoynadno@yandex.ru>
2023-05-14 11:48:53 +0300

    Change API URL

--- services/emptySplitApi.ts
+++ services/emptySplitApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-//http://somnoynadno.ru:8080/api/v1/
+//https://samokat.somnoynadno.ru/api/v1/
 export const emptySplitApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
     tagTypes: ["News", "Gifts", "Achievements", "Employee", "Events", "Units", "Tasks"],
--- services/realSplitApi.ts
+++ services/realSplitApi.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
-// http://somnoynadno.ru:8080/api/v
+// https://samokat.somnoynadno.ru/api/v
 // http://localhost:3000/api
 
 export const realSplitApi = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://somnoynadno.ru:8080/api/v1' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://samokat.somnoynadno.ru/api/v1' }),
     tagTypes: ["News", "Gifts", "Achievements", "Employee", "Events", "Units", "Tasks"],
     endpoints: () => ({}),
 })
